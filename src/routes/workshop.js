3c2a27d6364c94c87180528cf27a37365be905a1
Homerus Hoogland <hj.hoogland@student.avans.nl>
2024-06-04 14:57:56 +0200

    Workshop names get for website add commission

--- src/routes/workshop.js
+++ src/routes/workshop.js
@@ -64,4 +64,28 @@ router.get("/all", async (req, res) => {
     }
   });
 
+  /** Get all workshops route
+ *
+ *  Retrieves all workshops names from the database.
+ *  Used for the dropdown of the add commissions front-end
+ */
+router.get("/allnames", async (req, res) => {
+  console.log("GET /all");
+
+  try {
+    const pool = await poolPromise;
+    const query = "SELECT WorkshopName FROM [Workshop]";
+    console.log("EXECUTING QUERY ON DATABASE: " + query);
+    const result = await pool.request().query(query);
+    res.json({
+      status: 200,
+      data: result.recordset,
+      message: "Succesfully retrieved all workshop names",
+    });
+  } catch (error) {
+    console.error("Database query error:", error);
+    res.status(500).json({ error: "Database Query Error" });
+  }
+});
+
 module.exports = router;
