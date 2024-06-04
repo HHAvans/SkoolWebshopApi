6346793022b765edc64b0a3fb6ba5230797e3ff0
Shirley <saa.wenzel@student.avans.nl>
2024-06-04 12:34:41 +0200

    edited cors settings

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const { sql, poolPromise } = require("./src/dao/sqldao.js");
-const cors = require("cors");
+const cors = require('cors')
 const app = express();
 const path = require('path');
 
-app.use(cors({
-  origin: '*', // Allow all origins (you can restrict this to specific origins if needed)
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  preflightContinue: false,
-  optionsSuccessStatus: 204
-}));
+
+app.use(cors())
 app.use(express.json());
 
 
