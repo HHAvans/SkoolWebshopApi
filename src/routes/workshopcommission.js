73d14813018ecd5c78d7241899eec3d05d3ce7a7
Fleur <fleurmaxyara@gmail.com>
2024-06-06 02:57:24 +0200

    api /:id for my workshops

--- src/routes/workshopcommission.js
+++ src/routes/workshopcommission.js
@@ -48,4 +48,52 @@ router.get("/all", async (req, res) => {
   }
 });
 
+// krijg alle workshops waar de userid overeenkomt met header en is toegewezen
+router.get("/:id", async (req, res) => {
+  console.log("GET /:id");
+
+  try {
+    const pool = await poolPromise;
+    const query = `
+    SELECT 
+    CommissionWorkshopId, 
+    WorkshopName, 
+    CONVERT(VARCHAR(10), Date, 120) AS Date, 
+    CONVERT(VARCHAR(8), StartTime, 108) AS StartTime, 
+    CONVERT(VARCHAR(8), EndTime, 108) AS EndTime, 
+    Requirements, 
+    Category, 
+    CommissionName, 
+    Location, 
+    LinkToPicture 
+FROM 
+    CommissionWorkshop 
+INNER JOIN 
+    Commission 
+ON 
+    CommissionWorkshop.CommissionId = Commission.CommissionId 
+INNER JOIN 
+    Workshop 
+ON 
+    CommissionWorkshop.WorkshopId = Workshop.WorkshopId 
+WHERE 
+    CommissionWorkshop.userId = ${req.params.id};
+ORDER BY 
+    Date, 
+    StartTime;
+    `;
+    console.log("EXECUTING QUERY ON DATABASE: " + query);
+    const result = await pool.request().query(query);
+    res.json({
+      status: 200,
+      message: "Succesfully retrieved workshops in commissions and all associated data",
+      data: result.recordset,
+    });
+  } catch (error) {
+    console.error("Database query error:", error);
+    res.status(500).json({ error: "Database Query Error" });
+  }
+});
+
+
 module.exports = router;
