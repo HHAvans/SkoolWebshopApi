88c3b59639e86f3fe2dc00b93f01dcadd00d1459
Shirley <saa.wenzel@student.avans.nl>
2024-06-07 15:32:58 +0200

    Route requesting basic data from users

--- src/routes/user.js
+++ src/routes/user.js
@@ -28,7 +28,7 @@ router.get('/all', async (req, res) => {
         console.log('EXECUTING QUERY ON DATABASE: SELECT * FROM [User]')
         const result = await pool.request().query('SELECT * FROM [User]');
 
-         // Remove IDs and passwords from the result set
+        // Remove IDs and passwords from the result set
         const usersWithoutIdAndPassword = result.recordset.map(user => {
             const { UserId, Password, ...rest } = user;
             return rest;
@@ -43,6 +43,39 @@ router.get('/all', async (req, res) => {
         console.error('Database query error:', error);
         res.status(500).json({ error: 'Database Query Error' });
     }
-}); 
+});
+
+// fetching username, email and role 
+
+router.get('/basic', async (req, res) => {
+    console.log('GET /user/basic');
+
+    try {
+        const pool = await poolPromise;
+        const status = 'Afwachtend';
+        console.log('EXECUTING QUERY ON DATABASE: SELECT Username, Email, Role FROM [User] WHERE Status = @status');
+
+        const result = await pool.request()
+            .input('status', status)
+            .query('SELECT Username, Email, Role FROM [User] WHERE Status = @status');
+
+            if (result.recordset.length > 0) {
+                res.status(200).json({
+                    status: 200,
+                    message: "User data retrieved successfully",
+                    data: result.recordset
+                });
+            } else {
+                res.status(404).json({
+                    status: 404,
+                    message: "No users found with the given status"
+                });
+            }
+        } catch (error) {
+            console.error('Database query error:', error);
+            res.status(500).json({ error: 'Database Query Error' });
+        }
+    });
+
 
 module.exports = router;
