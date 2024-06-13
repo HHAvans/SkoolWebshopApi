389147fc298ce74fa1d74f4b17d334d42ff94be6
Homerus Hoogland <hj.hoogland@student.avans.nl>
2024-06-13 08:53:47 +0200

    Api fix

--- src/services/scheduler.js
+++ src/services/scheduler.js
@@ -1,6 +1,6 @@
 const cron = require('node-cron');
 const { sql, poolPromise } = require('../dao/sqldao'); // Adjust the path as necessary
-const { sendEmail, wrapInHtml } = require('../services/emailService');
+const { sendEmail, wrapInHtml } = require('./emailService');
 
 // Function to replace placeholders in the template with actual data
 function replacePlaceholders(template, data) {
@@ -45,7 +45,7 @@ async function sendWorkshopReminders() {
                 Workshop w ON cw.WorkshopId = w.WorkshopId
             WHERE 
                 CAST(CONCAT(CONVERT(VARCHAR(10), c.Date, 120), ' ', CONVERT(VARCHAR(8), cw.StartTime, 108)) AS DATETIME) 
-                BETWEEN DATEADD(HOUR, 24, GETDATE()) AND DATEADD(HOUR, 48, GETDATE());
+                BETWEEN DATEADD(HOUR, 1, GETDATE()) AND DATEADD(HOUR, 48, GETDATE());
         `;
 
         const workshopResult = await pool.request().query(workshopQuery);
@@ -99,7 +99,7 @@ async function sendWorkshopReminders() {
 }
 
 // Schedule the job to run once a day at midnight
-cron.schedule('0 0 * * *', () => {
+cron.schedule('* * * * *', () => {
     console.log("Running workshop reminder job once a day at midnight");
     sendWorkshopReminders();
 });
